eef46759a18d1a2dfb2aa66492dca4043c471b78
Mykola Shumyn <msh@adorsys.com.ua>
2020-05-03 17:04:27 +0300

    Implemented elasticsearch create and get operations

--- services/items_service.go
+++ services/items_service.go
@@ -12,14 +12,28 @@ var (
 type itemsServiceInterface interface {
 	Create(items.Item) (*items.Item, rest_errors.RestErr)
 	Get(string) (*items.Item, rest_errors.RestErr)
+	//Search(queries.EsQuery) ([]items.Item, rest_errors.RestErr)
 }
 
 type itemsService struct{}
 
 func (s *itemsService) Create(item items.Item) (*items.Item, rest_errors.RestErr) {
-	return nil, nil
+	if err := item.Save(); err != nil {
+		return nil, err
+	}
+	return &item, nil
 }
 
 func (s *itemsService) Get(id string) (*items.Item, rest_errors.RestErr) {
-	return nil, nil
+	item := items.Item{Id: id}
+
+	if err := item.Get(); err != nil {
+		return nil, err
+	}
+	return &item, nil
 }
+
+//func (s *itemsService) Search(query queries.EsQuery) ([]items.Item, rest_errors.RestErr) {
+//	dao := items.Item{}
+//	return dao.Search(query)
+//}
